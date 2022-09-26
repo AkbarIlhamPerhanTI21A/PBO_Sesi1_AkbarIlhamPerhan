4d09edc58a61462e55ceb102fccba0a49edee75c
AkbarIlhamPerhanTI21A <114407901+AkbarIlhamPerhanTI21A@users.noreply.github.com>
2022-09-26 20:27:14 +0700

    Add files via upload

---
+++ Sesi1No1.java
@@ -0,0 +1,13 @@
+/**
+ * PBO Sesi 1 Nomor 1
+ * Akbar Ilham Perhan
+ * 20210040083
+ * TI21A
+ */
+public class Sesi1No1 {
+    public static void main(String[] args) {
+        System.out.println("Belajar Java");
+        System.out.println("Sangat mudah sekali");
+        System.out.println("dan sangat menyenangkan");
+    }
+}
---
+++ Sesi1No2.java
@@ -0,0 +1,16 @@
+/**
+ * PBO Sesi 1 Nomor 2
+ * Akbar Ilham Perhan
+ * 20210040083
+ * TI21A
+ */
+import javax.swing.JOptionPane;
+
+public class Sesi1No2 {
+    public static void main(String[] args) {
+   String  belajar;
+   belajar  =   JOptionPane.showInputDialog("Anda sedang belajar apa?");
+   
+        System.out.println("belajar "+belajar+" sangat mudah");
+    }
+}
